b179cb11502bcff26a74bc455b815a6d88a1a2d2
bryce <bryce@10.0.1.13>
2014-12-21 14:04:56 -0500

    Add imports to GCClientSocket

---
+++ GovChat-Dev/src/edu/govschool/govchat/socket/GCClientSocket.java
@@ -0,0 +1,17 @@
+package edu.govschool.govchat.socket;
+
+// For IOExceptions
+import java.io.IOException;
+// For socket classes
+import java.net.*;
+// For Platform
+import javafx.application.Platform;
+
+/**
+ *
+ * @author Mr. Davis
+ */
+public class GCClientSocket 
+{
+
+}
