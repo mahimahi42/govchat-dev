acb81ef1114318d0ab41907c04b39fc4895b2cd3
Bryce Davis <me@bryceadavis.com>
2014-12-21 14:30:31 -0500

    Finish GCClientSocket implementation

--- GovChat-Dev/src/edu/govschool/govchat/socket/GCClientSocket.java
+++ GovChat-Dev/src/edu/govschool/govchat/socket/GCClientSocket.java
@@ -30,6 +30,13 @@ implements GCSocketListener
     // This instance of our interface will talk to JavaFX for us
     private GCSocketListener fxListener;
     
+    /**
+     * Default constructor.
+     * Create a new client socket bound to the specified host and port
+     * @param fxListener the listener instance to talk to our GUI
+     * @param host the host to connect to
+     * @param port the port to bind to
+     */
     public GCClientSocket(GCSocketListener fxListener,
                           String host,
                           int port)
@@ -39,24 +46,65 @@ implements GCSocketListener
         this.fxListener = fxListener;
     }
     
+    /**
+     * Debug constructor.
+     * Create a new client socket connected to <code>localhost</code> on port 
+     * 2000
+     * @param fxListener the listener instance to talk to our GUI
+     */
+    public GCClientSocket(GCSocketListener fxListener)
+    {
+        this(fxListener, DEFAULT_HOST, DEFAULT_PORT);
+    }
+    
+    /**
+     * Read a message from the socket.
+     * We use <code>Platform</code> to send our message to the GUI.
+     * @param line the message received
+     */
     @Override
     public void onMessage(final String line)
     {
-        
+        Platform.runLater(() -> fxListener.onMessage(line));
     }
     
+    /**
+     * Change the connection status of the socket.
+     * <code>Platform</code> is again used to inform our GUI.
+     * @param isClosed the new connection status of the socket
+     */
     @Override
     public void onCloseUpdate(final Boolean isClosed)
     {
-        
+        Platform.runLater(() -> fxListener.onCloseUpdate(isClosed));
     }
     
+    /**
+     * Initialize a client connection.
+     * We connect to the <code>GCServerSocket</code> listening on the host and 
+     * port specified in the constructor.
+     * @throws SocketException the connection failed to connect
+     */
     @Override
     public void initConnection() throws SocketException
     {
-        
+        try {
+            // Create our only socket
+            socketConnection = new Socket();
+            // Allow us to connect if another client fails to connect
+            socketConnection.setReuseAddress(true);
+            // Create the connection
+            socketConnection.connect(new InetSocketAddress(host, port));
+            System.out.println("Connected to: " + host + ":" + port);
+        } catch (Exception e) {
+            throw new SocketException();
+        }
     }
     
+    /**
+     * Null method.
+     * We have no extra sockets, so this method doesn't need an implementation.
+     */
     @Override
     public void closeAdditionalSockets() {}
 }
