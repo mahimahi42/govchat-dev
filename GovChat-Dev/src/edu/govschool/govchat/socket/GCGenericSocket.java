4eda5045afbd9f454e21a531678e4da4f90f059c
bryce <bryce@10.0.1.13>
2014-12-21 13:01:19 -0500

    Add properties for GCGenericSocket

--- GovChat-Dev/src/edu/govschool/govchat/socket/GCGenericSocket.java
+++ GovChat-Dev/src/edu/govschool/govchat/socket/GCGenericSocket.java
@@ -18,5 +18,24 @@ import javafx.application.Platform;
  */
 public abstract class GCGenericSocket implements GCSocketListener
 {
-
+    // For testing purposes, we'll use a default port of 2000.
+    // It is defined here in the superclass because both the
+    // client and server need to agree on the port.
+    public static final int DEFAULT_PORT = 2000;
+    
+    // The port we'll be using
+    public int port;
+    // The Socket connection we'll be using
+    // `protected` means that we and our subclasses can access this property, 
+    // no classes outside us.
+    protected Socket socketConncetion = null;
+    // Streams to perform I/O
+    private BufferedWriter output = null;
+    private BufferedReader input = null;
+    // Flag to see if our setup has completed
+    private boolean ready = false;
+    // Thread to setup our connection
+    private Thread setupThread;
+    // Thread to read message from our connection
+    private Thread readerThread;
 }
