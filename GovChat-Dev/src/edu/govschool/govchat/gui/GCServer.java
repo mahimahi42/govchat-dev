35c882bc95a377d45147a51920f7a4c13db262a6
Bryce Davis <me@bryceadavis.com>
2014-12-21 14:56:29 -0500

    Add JavaFX method stubs to GCServer

---
+++ GovChat-Dev/src/edu/govschool/govchat/gui/GCServer.java
@@ -0,0 +1,32 @@
+package edu.govschool.govchat.gui;
+
+// We need to import our socket package
+import edu.govschool.govchat.socket.*;
+// JavaFX imports
+import javafx.application.*;
+import javafx.geometry.*;
+import javafx.scene.*;
+import javafx.scene.control.*;
+import javafx.scene.layout.*;
+import javafx.stage.*;
+
+/**
+ * GUI for the GovChat server.
+ * Most of the actual work is handled with <code>GCServerSocket</code>, this
+ * provides us with a GUI to work with this socket and the entire server in
+ * general.
+ * @author Mr. Davis
+ */
+public class GCServer extends Application
+{
+    public static void main(String[] args)
+    {
+        
+    }
+    
+    @Override
+    public void start(Stage primaryStage)
+    {
+        
+    }
+}
