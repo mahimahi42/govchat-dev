69731513cd4a2a7ab29590666e51b80f0ea76600
Bryce Davis <me@bryceadavis.com>
2014-12-21 15:11:29 -0500

    Add properties to GCServer

--- GovChat-Dev/src/edu/govschool/govchat/gui/GCServer.java
+++ GovChat-Dev/src/edu/govschool/govchat/gui/GCServer.java
@@ -19,9 +19,26 @@ import javafx.stage.*;
  */
 public class GCServer extends Application
 {
+    // Our GUI initially is very spartan, consisting of a large text area to
+    // display messages sent and received, a entry field to enter messages, a
+    // connect button and a send button. At first, we just connect using the
+    // default host and port, localhost and 2000, so we don't need any elements
+    // to input a custom host and port yet.
+    private TextArea msgArea;
+    private TextField entryField;
+    private Button sendBtn;
+    private Button connectBtn;
+    
+    // Since this is the server GUI, we need a server socket!
+    private GCServerSocket serverSocket;
+    
+    // Some flags to represent several states our application can be in
+    private boolean socketClosed = true;
+    private boolean tryingToConnect = false;
+    
     public static void main(String[] args)
     {
-        
+        launch(args);
     }
     
     @Override
