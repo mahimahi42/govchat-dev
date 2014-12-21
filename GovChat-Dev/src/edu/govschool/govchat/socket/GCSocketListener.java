22fc791a5db90d5c6bae943170659ff255cb2307
bryce <bryce@10.0.1.13>
2014-12-21 12:55:19 -0500

    Update GCSocketListener comments to better explain necessities for JavaFX thread-safety

--- GovChat-Dev/src/edu/govschool/govchat/socket/GCSocketListener.java
+++ GovChat-Dev/src/edu/govschool/govchat/socket/GCSocketListener.java
@@ -5,7 +5,9 @@ package edu.govschool.govchat.socket;
  * Based on Oracle documentation. While our two sockets will be inheriting from
  * <code>GCGenericSocket</code> to share setup code, this interface will define
  * a few methods that the sockets will use differently depending on what they
- * are listening for.
+ * are listening for. Additionally, these messages will interact somehow with
+ * the JavaFX aspect of our application, so we have to deal with them 
+ * individually.
  * @author Mr. Davis
  */
 public interface GCSocketListener 
