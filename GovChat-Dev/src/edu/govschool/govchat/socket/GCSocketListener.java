c47e6c645f83ceac11a929225a873acb8484618e
bryce <bryce@10.0.1.13>
2014-12-21 12:43:57 -0500

    Add file stub for socket listener

---
+++ GovChat-Dev/src/edu/govschool/govchat/socket/GCSocketListener.java
@@ -0,0 +1,14 @@
+package edu.govschool.govchat.socket;
+
+/**
+ * Listener interface for sockets used in GovChat.
+ * Based on Oracle documentation. While our two sockets will be inheriting from
+ * <code>GCGenericSocket</code> to share setup code, this interface will define
+ * a few methods that the sockets will use differently depending on what they
+ * are listening for.
+ * @author Mr. Davis
+ */
+public interface GCSocketListener 
+{
+
+}
