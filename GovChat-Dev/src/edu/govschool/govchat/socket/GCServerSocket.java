791cb9aa8932801958384005b6af1c2a391bd252
bryce <bryce@10.0.1.13>
2014-12-21 13:39:56 -0500

    Add stub for GCServerSocket

---
+++ GovChat-Dev/src/edu/govschool/govchat/socket/GCServerSocket.java
@@ -0,0 +1,18 @@
+package edu.govschool.govchat.socket;
+
+// For socket classes
+import java.net.*;
+// For Platform
+import javafx.application.Platform;
+
+/**
+ * Server socket implementation for GovChat.
+ * Based on Oracle documentation.
+ * This socket will act as the central server for our application. Our server
+ * will instantiate an instance of this class for use.
+ * @author Mr. Davis
+ */
+public class GCServerSocket 
+{
+
+}
