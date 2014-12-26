647acf4b031081fd30b170a366fcdebf13d2d115
Bryce Davis <bad1128@gmail.com>
2014-12-26 16:11:26 -0500

    GCLauncher can now launch both GCServer and GCClient

--- GovChat-Dev/src/edu/govschool/govchat/gui/GCLauncher.java
+++ GovChat-Dev/src/edu/govschool/govchat/gui/GCLauncher.java
@@ -1,8 +1,10 @@
 package edu.govschool.govchat.gui;
 
 import javafx.application.Application;
+import javafx.application.Platform;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 /**
@@ -13,6 +15,11 @@ import javafx.stage.Stage;
  */
 public class GCLauncher extends Application
 {
+    // The button to launch a server instance
+    public static Button serverBtn;
+    // The button to launch a client instance
+    public static Button clientBtn;
+    
     public static void main(String[] args)
     {
         launch(args);
@@ -21,6 +28,25 @@ public class GCLauncher extends Application
     @Override
     public void start(Stage primaryStage)
     {
+        // Create our buttons
+        serverBtn = new Button("Launch Server");
+        clientBtn = new Button("Launch Client");
         
+        // Set our buttons' actions
+        serverBtn.setOnAction(e -> {
+            Platform.runLater(() -> new GCServer().start(new Stage()));
+        });
+        clientBtn.setOnAction(e -> {
+            Platform.runLater(() -> new GCClient().start(new Stage()));
+        });
+        
+        // Organize our window
+        VBox pane = new VBox(10, serverBtn, clientBtn);
+        Scene scene = new Scene(pane);
+        
+        // Create and show our window
+        primaryStage.setScene(scene);
+        primaryStage.setTitle("GovChat Launcher");
+        primaryStage.show();
     }
 }
