06f459362b914c7cc3ba70e7cf09864496e1ec22
Bryce Davis <bad1128@gmail.com>
2014-12-26 15:22:16 -0500

    Add username property and basic constructors to GCUser.

--- GovChat-Dev/src/edu/govschool/govchat/user/GCUser.java
+++ GovChat-Dev/src/edu/govschool/govchat/user/GCUser.java
@@ -1,12 +1,37 @@
 package edu.govschool.govchat.user;
 
-
+import java.io.Serializable;
 
 /**
- *
- * @author bryce
+ * This class represents a connected user to GovChat.
+ * This class will store the entire representation of a connected user. This
+ * includes any data or methods needed on this object. For example, the user's
+ * nickname (or username) and favorite servers could be stored here. The class
+ * implements the interface <code>Serializable</code> to allow it to be written
+ * to a file.
+ * @author Mr. Davis
  */
-public class GCUser 
+public class GCUser implements Serializable
 {
-
+    // The username of the user
+    private String username;
+    
+    /**
+     * Empty constructor.
+     * Creates a <code>GCUser</code> with empty information.
+     */
+    public GCUser()
+    {
+        this("");
+    }
+    
+    /**
+     * Default constructor.
+     * Creates a <code>GCUser</code> with the given username.
+     * @param username the username of the <code>GCUser</code>
+     */
+    public GCUser(String username)
+    {
+        this.username = username;
+    }
 }
