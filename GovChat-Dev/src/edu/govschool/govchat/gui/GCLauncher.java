d40f348235fa9abd27dbcf892bd05b780b9665d3
Bryce Davis <bad1128@gmail.com>
2014-12-26 16:04:30 -0500

    Add JavaFX method stubs to GCLauncher

--- GovChat-Dev/src/edu/govschool/govchat/gui/GCLauncher.java
+++ GovChat-Dev/src/edu/govschool/govchat/gui/GCLauncher.java
@@ -1,12 +1,26 @@
 package edu.govschool.govchat.gui;
 
-
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
 
 /**
- *
- * @author bryce
+ * Application to easily launch a GovChat client or server.
+ * This launcher window will allow the user to setup their user, and then launch
+ * a GovChat server or client instance.
+ * @author Mr. Davis
  */
-public class GCLauncher 
+public class GCLauncher extends Application
 {
-
+    public static void main(String[] args)
+    {
+        launch(args);
+    }
+    
+    @Override
+    public void start(Stage primaryStage)
+    {
+        
+    }
 }
